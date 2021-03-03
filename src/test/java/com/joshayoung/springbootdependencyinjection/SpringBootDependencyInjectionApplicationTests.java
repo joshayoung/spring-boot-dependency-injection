b0de7c44f0f0d0ebc22c4a66d10430f3775dd6cf
Josh Young <j22cal@gmail.com>
2021-03-03 13:03:19 -0600

    Add initializr files

---
+++ src/test/java/com/joshayoung/springbootdependencyinjection/SpringBootDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshayoung.springbootdependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
