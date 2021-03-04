6c92ef3add2e8c768d0320a600b26ac35ea84008
Josh Young <j22cal@gmail.com>
2021-03-04 12:26:09 -0600

    Add annotation for property based injection

--- src/main/java/com/joshayoung/springbootdependencyinjection/SpringBootDependencyInjectionApplication.java
+++ src/main/java/com/joshayoung/springbootdependencyinjection/SpringBootDependencyInjectionApplication.java
@@ -1,6 +1,7 @@
 package com.joshayoung.springbootdependencyinjection;
 
 import com.joshayoung.springbootdependencyinjection.controllers.MyController;
+import com.joshayoung.springbootdependencyinjection.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +16,11 @@ public class SpringBootDependencyInjectionApplication {
         String greeting = myController.sayHello();
 
         System.out.println(greeting);
+
+        System.out.println("------- Property");
+
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+        System.out.println(propertyInjectedController.getGreeting());
     }
 
 }
--- src/main/java/com/joshayoung/springbootdependencyinjection/services/GreetingServiceImpl.java
+++ src/main/java/com/joshayoung/springbootdependencyinjection/services/GreetingServiceImpl.java
@@ -1,5 +1,8 @@
 package com.joshayoung.springbootdependencyinjection.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
