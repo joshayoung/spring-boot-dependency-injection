928f67938a935cb55d0155919e4d1ffae633d399
Josh Young <j22cal@gmail.com>
2021-03-04 12:09:49 -0600

    Setter Injection and test

---
+++ src/main/java/com/joshayoung/springbootdependencyinjection/controllers/SetterInjectedController.java
@@ -0,0 +1,15 @@
+package com.joshayoung.springbootdependencyinjection.controllers;
+
+import com.joshayoung.springbootdependencyinjection.services.GreetingService;
+
+public class SetterInjectedController {
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
