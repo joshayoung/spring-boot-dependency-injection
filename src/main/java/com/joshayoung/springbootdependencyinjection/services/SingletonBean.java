26687734f4437adff5ca10dfd5416d5dcc77ac3a
Josh Young <j22cal@gmail.com>
2021-03-17 07:35:45 -0500

    Singleton vs Prototype

---
+++ src/main/java/com/joshayoung/springbootdependencyinjection/services/SingletonBean.java
@@ -0,0 +1,14 @@
+package com.joshayoung.springbootdependencyinjection.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SingletonBean {
+    public SingletonBean() {
+        System.out.println("Creating a Singleton Bean");
+    }
+
+    public String getMyScope() {
+       return "I'm a Singleton";
+    }
+}
